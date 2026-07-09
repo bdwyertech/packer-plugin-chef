7c3927506a6859be85ba434fae01cb6b8cce9a6c
Brian Dwyer <brian.dwyer@broadridge.com>
2026-07-09 09:51:30 -0400

    Fix

--- provisioner/chef-client/provisioner.hcl2spec.go
+++ provisioner/chef-client/provisioner.hcl2spec.go
@@ -34,7 +34,7 @@ type FlatConfig struct {
 	PolicyGroup                *string           `mapstructure:"policy_group" cty:"policy_group" hcl:"policy_group"`
 	PolicyName                 *string           `mapstructure:"policy_name" cty:"policy_name" hcl:"policy_name"`
 	PreventSudo                *bool             `mapstructure:"prevent_sudo" cty:"prevent_sudo" hcl:"prevent_sudo"`
-	RetryOnExitCode            map[string]bool   `mapstructure:"retry_on_exit_code" cty:"retry_on_exit_code" hcl:"retry_on_exit_code"`
+	RetryOnExitCode            []int             `mapstructure:"retry_on_exit_code" cty:"retry_on_exit_code" hcl:"retry_on_exit_code"`
 	RubygemsURL                *string           `mapstructure:"rubygems_url" cty:"rubygems_url" hcl:"rubygems_url"`
 	RunList                    []string          `mapstructure:"run_list" cty:"run_list" hcl:"run_list"`
 	ServerUrl                  *string           `mapstructure:"server_url" cty:"server_url" hcl:"server_url"`
@@ -88,7 +88,7 @@ func (*FlatConfig) HCL2Spec() map[string]hcldec.Spec {
 		"policy_group":                   &hcldec.AttrSpec{Name: "policy_group", Type: cty.String, Required: false},
 		"policy_name":                    &hcldec.AttrSpec{Name: "policy_name", Type: cty.String, Required: false},
 		"prevent_sudo":                   &hcldec.AttrSpec{Name: "prevent_sudo", Type: cty.Bool, Required: false},
-		"retry_on_exit_code":             &hcldec.AttrSpec{Name: "retry_on_exit_code", Type: cty.Map(cty.String), Required: false},
+		"retry_on_exit_code":             &hcldec.AttrSpec{Name: "retry_on_exit_code", Type: cty.List(cty.Number), Required: false},
 		"rubygems_url":                   &hcldec.AttrSpec{Name: "rubygems_url", Type: cty.String, Required: false},
 		"run_list":                       &hcldec.AttrSpec{Name: "run_list", Type: cty.List(cty.String), Required: false},
 		"server_url":                     &hcldec.AttrSpec{Name: "server_url", Type: cty.String, Required: false},
--- provisioner/chef-solo/provisioner.hcl2spec.go
+++ provisioner/chef-solo/provisioner.hcl2spec.go
@@ -31,7 +31,7 @@ type FlatConfig struct {
 	RemoteCookbookPaths        []string          `mapstructure:"remote_cookbook_paths" cty:"remote_cookbook_paths" hcl:"remote_cookbook_paths"`
 	JsonString                 *string           `mapstructure:"json_string" cty:"json_string" hcl:"json_string"`
 	PreventSudo                *bool             `mapstructure:"prevent_sudo" cty:"prevent_sudo" hcl:"prevent_sudo"`
-	RetryOnExitCode            map[string]bool   `mapstructure:"retry_on_exit_code" cty:"retry_on_exit_code" hcl:"retry_on_exit_code"`
+	RetryOnExitCode            []int             `mapstructure:"retry_on_exit_code" cty:"retry_on_exit_code" hcl:"retry_on_exit_code"`
 	WaitForRetry               *string           `mapstructure:"wait_for_retry" cty:"wait_for_retry" hcl:"wait_for_retry"`
 	OmnitruckUrl               *string           `mapstructure:"omnitruck_url" cty:"omnitruck_url" hcl:"omnitruck_url"`
 	RunList                    []string          `mapstructure:"run_list" cty:"run_list" hcl:"run_list"`
@@ -74,7 +74,7 @@ func (*FlatConfig) HCL2Spec() map[string]hcldec.Spec {
 		"remote_cookbook_paths":          &hcldec.AttrSpec{Name: "remote_cookbook_paths", Type: cty.List(cty.String), Required: false},
 		"json_string":                    &hcldec.AttrSpec{Name: "json_string", Type: cty.String, Required: false},
 		"prevent_sudo":                   &hcldec.AttrSpec{Name: "prevent_sudo", Type: cty.Bool, Required: false},
-		"retry_on_exit_code":             &hcldec.AttrSpec{Name: "retry_on_exit_code", Type: cty.Map(cty.String), Required: false},
+		"retry_on_exit_code":             &hcldec.AttrSpec{Name: "retry_on_exit_code", Type: cty.List(cty.Number), Required: false},
 		"wait_for_retry":                 &hcldec.AttrSpec{Name: "wait_for_retry", Type: cty.String, Required: false},
 		"omnitruck_url":                  &hcldec.AttrSpec{Name: "omnitruck_url", Type: cty.String, Required: false},
 		"run_list":                       &hcldec.AttrSpec{Name: "run_list", Type: cty.List(cty.String), Required: false},
